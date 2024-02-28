0e6313195d4a5ca4aeba422dc6aba07e6c9d4157
BobSol02 <bobansolomou@gmail.com>
2024-02-28 19:57:21 +0200

    Added startsOn and endsOn functionality on app page

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ document.addEventListener('DOMContentLoaded', function refresh () {
         try {
             const response = await fetch("https://codecyprus.org/th/api/list");
             const jsonObject = await response.json();
-
             let treasureHuntsArray = jsonObject.treasureHunts;
+            console.log(jsonObject);
             let treasurehuntList = document.getElementById("treasurehuntList");
 
             for (let i = 0; i < treasureHuntsArray.length; i++) {
                 let listItem = document.createElement('li');
-                listItem.textContent = treasureHuntsArray[i].name;
+                let start = handleTime(treasureHuntsArray[i].startsOn);
+                let end = handleTime(treasureHuntsArray[i].endsOn);
+                listItem.innerHTML = treasureHuntsArray[i].name+"</br>"+"Starts in: "+start+"</br>"+"Ends in: "+end;
                 treasurehuntList.appendChild(listItem);
             }
         } catch (error) {
@@ -23,3 +25,10 @@ document.addEventListener('DOMContentLoaded', function refresh () {
         location.reload();
     });
 });
+
+function handleTime(unixTime){
+    let options = {day:"numeric",month:"short",year:"numeric",hour:"2-digit",minute:"2-digit",second:"2-digit"};
+    let dateAndTime = new Date(unixTime);
+    let formattedDateAndTime = dateAndTime.toLocaleDateString("en-UK",options);
+    return formattedDateAndTime;
+}
